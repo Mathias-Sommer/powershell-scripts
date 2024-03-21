c87a28cf51c0cbe86bceb64a0df53c07517733e7
Mathias Sommer <160283514+Mathias-Sommer@users.noreply.github.com>
2024-03-21 21:31:26 +0100

    Update ADGetMembersOfGroup(s).ps1
    Improved code

--- ADGetMembersOfGroup(s).ps1
+++ ADGetMembersOfGroup(s).ps1
@@ -5,44 +5,33 @@
 Import-Module ImportExcel
 
 # Specify the starting string for group names
-$groupNamePrefix = "INSERTGROUPNAME/PREFIXFORGROUPS"
+$groupNamePrefix = "admin"
 
-# Get groups that start with the specified prefix
 $groups = Get-ADGroup -Filter "Name -like '$groupNamePrefix*'" -Properties *
-
-# Create an array to store the results
 $results = @()
-
-# Loop through each group
 foreach ($group in $groups) {
-    # Get members of the current group
     $members = Get-ADGroupMember -Identity $group
 
-    # Loop through each member of the group
     foreach ($member in $members) {
 
     if($group.ManagedBy -eq $null){
         $manager = $false
-        }
-        else{
-        $manager = Get-ADUser -Identity $group.ManagedBy 
-        }
+        } else{
+            $manager = Get-ADUser -Identity $group.ManagedBy 
+            }
 
-        # Create an object with group and member details
         $resultObject = [PSCustomObject]@{
             'GroupName' = $group.Name
             'MemberName' = $member.Name
             'PUID' = if ($member.SamAccountName -ne $member.name) { $member.SamAccountName } else { "" }
-            'Manager' = if ($manager) { $manager.Name } else { "" }
-        }
+            'Manager' = if ($manager) { $manager.Name } else { "No Manager" } 
+            }
 
-        # Add the object to the results array
-        $results += $resultObject
-    }
+         $results += $resultObject
+        }
 }
 
-# Print results to console
 $results | Format-Table -AutoSize
 
-# Export results to CSV
-$results | Export-Excel "${groupNamePrefix}.xlsx" -Append -WorksheetName "$groupNamePrefix" -TableStyle Medium9 -title "$groupNamePrefix" -TitleBold -AutoSize
+# Export results to excel
+$results | Export-Excel -Path C:\Temp\${groupNamePrefix}.xlsx -Append -WorksheetName "1" -TableStyle Medium9 -AutoSize
