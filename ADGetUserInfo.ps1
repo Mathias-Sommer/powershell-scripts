1d25fcea7c984ecfa34c47fb07b77004530e70c9
Mathias Sommer <160283514+Mathias-Sommer@users.noreply.github.com>
2024-03-29 20:30:02 +0100

    Update ADGetUserInfo.ps1
    Added lots of if statements because I'm a pleb.
    This is because someone was experiencing errors when a value was $null. So I've added checks to handle this.

--- ADGetUserInfo.ps1
+++ ADGetUserInfo.ps1
@@ -1,7 +1,7 @@
 #This gets useful information of a user.
 #**Note if your domain controllers haven't synced, then the date/time can way be off. Please keep that in mind.
 
-$user = Get-ADUser -Filter {SamAccountName -eq "ENTER_SAMACCOUNTNAME"} -Properties lastLogon, WhenCreated, PwdLastSet, Manager | Select-Object Name, SamAccountName, @{
+$user = Get-ADUser -Filter {SamAccountName -eq "hans"} -Properties lastLogon, WhenCreated, PwdLastSet, Manager | Select-Object Name, SamAccountName, @{
     Name="LastLogon"; 
     Expression={[DateTime]::FromFileTime($_.lastLogon)}
 }, @{
@@ -12,13 +12,46 @@ $user = Get-ADUser -Filter {SamAccountName -eq "ENTER_SAMACCOUNTNAME"} -Properti
     Expression={[DateTime]::FromFileTime($_.PwdLastSet)}
 }, Manager
 
-$manager = Get-ADUser $user.Manager -Properties Name
-
 Write-host "User Information:"
-Write-host "------------------"
-Write-host "Name           : $($user.Name)" -ForegroundColor Green
-Write-host "SamAccountName : $($user.SamAccountName)" -ForegroundColor Yellow
-Write-host "LastLogon      : $($user.LastLogon.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
-Write-host "WhenCreated    : $($user.WhenCreated.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
-Write-host "PwdLastSet     : $($user.PwdLastSet.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
-Write-host "Manager        : $($manager.Name) $($manager.SamAccountName)" -ForegroundColor DarkGray
+Write-host "------------------" -ForegroundColor Gray
+
+if($user.Name -eq $null){
+    Write-host "Name           : N/A" -ForegroundColor gray
+    } else{
+        Write-host "Name           : $($user.Name)" -ForegroundColor Green
+        }
+
+if($user.SamAccountName -eq $null){
+    Write-host "SamAccountName : N/A" -ForegroundColor gray
+    } else{
+        Write-host "SamAccountName : $($user.SamAccountName)" -ForegroundColor Green
+        }
+
+if($user.Manager -eq $null){
+    Write-host "Manager        : Never" -ForegroundColor gray
+    }
+    else{
+        $manager = Get-ADUser $user.Manager -Properties Name, EmailAddress, TelephoneNumber
+        $user.Manager = $manager.Name + " | " + $manager.SamAccountName + " | " + $manager.EmailAddress + " | " + $manager.TelephoneNumber
+        Write-host "Manager        : $($user.Manager)" -ForegroundColor green
+        }
+
+if($user.LastLogon -eq $null -or $user.LastLogon -eq '01-01-1601 01:00:00'){
+    Write-host "LastLogon      : Never" -ForegroundColor gray
+    } else{
+        Write-host "LastLogon      : $($user.LastLogon.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
+        }
+
+if($user.WhenCreated -eq $null -or $user.WhenCreated -eq '01-01-1601 01:00:00'){
+    Write-host "WhenCreated    : N/A" -ForegroundColor gray
+    }
+    else{
+        Write-host "WhenCreated    : $($user.WhenCreated.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
+        }
+
+if($user.PwdLastSet -eq $null -or $user.PwdLastSet -eq '01-01-1601 01:00:00'){
+    Write-host "PwdLastSet     : Never" -ForegroundColor gray
+    }
+    else{
+        Write-host "PwdLastSet     : $($user.PwdLastSet.ToString("dd-MM-yyyy HH:mm:ss"))" -ForegroundColor Cyan
+        }
